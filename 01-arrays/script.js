69de1ec86a949cda5c9d1c3eb25619e07c3a9b51
Michelangela Paesano <paesanomichelangela@gmail.com>
2025-05-15 18:20:47 +0200

    esercizi finiti

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -37,8 +37,19 @@ console.log(teachers)
 
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
-const lewisIndex = null;
+let lewisIndex = null;
+
+for (let i = 0; i < teachers.length; i++) {
+  const currentTeacher = teachers[i]
+  if (currentTeacher === "Lewis") {
+    lewisIndex = i
+  }
+  console.log(lewisIndex)
+
+}
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty = null;
+const isTeachersEmpty = teachers.length === 0;
+console.log (isTeachersEmpty)
+
